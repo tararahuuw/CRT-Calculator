98208abbf40bc16ef13e92d35c5932fd333d61b9
Muhammad Fahmi Alamsyah <49779495+tararahuuw@users.noreply.github.com>
2021-06-05 16:10:26 +0700

    final

--- CRTCalc/src/components/FormCalc.js
+++ CRTCalc/src/components/FormCalc.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import "./FormCalc.css"
 
 const FormCalc = () => {
+    // UseState of FormCalc
     const [input1, setInput1] = useState('')
     const [input2, setInput2] = useState('')
     const [data, setData] = useState('')
@@ -10,13 +11,17 @@ const FormCalc = () => {
     const [totalA, setTotalA] = useState("")
     const [totalB, setTotalB] = useState("")
 
+    // Method for prevent reloading page
     const handleSubmit = (e) => {
         e.preventDefault();
     }
 
+    // Method for parsing text to integer
     function ChangeFormToInt(num) {
         return parseInt(num);
-      }
+    }
+
+    // Method for cleaning data form from comma and spacing
     function CleanDataForm(text) {
         let spliting = text.replace(/\s/g, '');
         spliting = spliting.split(",");
@@ -24,6 +29,7 @@ const FormCalc = () => {
         return spliting
     }
 
+    // Method for searching gcd with more than two number
     function gcd_more_than_two_numbers(input) { 
         var len, a, b;
           len = input.length;
@@ -39,6 +45,7 @@ const FormCalc = () => {
           return a;
       }
 
+    // Method for searching gcd with two number
     function gcd_two_numbers(x, y) {
 
         x = Math.abs(x);
@@ -51,31 +58,7 @@ const FormCalc = () => {
         return x;
     }
 
-    function display () {
-        var data1 = CleanDataForm(input1)
-        var data2 = CleanDataForm(input2)
-
-        // Check GCD of b
-        if (data2.length === data1.length) {
-            if (gcd_more_than_two_numbers(data2) === 1) {
-                setData (x => findMinX(data2, data1, data1.length) )
-                setSucces (x => true)
-            }
-            else {
-                alert("Komponen-komponen b harus saling relatif prima")
-                setSucces (x => false)
-            }
-        }
-        else {
-            alert("Jumlah komponen b dan a tidak sama")
-            setSucces (x => false)
-        }
-
-        // setTotalB (x => total (data2, data1, data3))
-        
-        setShow (x => true)
-    }
-
+    // Method for algorithm CRT
     function inv(a, m) {
      
         var m0 = m
@@ -125,24 +108,33 @@ const FormCalc = () => {
         
         return result % prod
     }
+    // End of method CRT algorithm
 
-    // function LoopTest() 
-    // {
-    //     var i=0;
-    //     var stop=5;
-    //     for (i=0;i<5;i++)
-    //     {
-    //         document.getElementById("container").innerHTML += "<button>button</button>"
-    //     }
-    // }
-    // function LoopTest() {
-    //     var i=0;
-    //     var stop=5;
-    
-    //     for (i=0;i<5;i++){  
-    //         document.getElementById("container").innerHTML += "<button>button</button>" 
-    //     }
-    // }
+    // Method for displaying result of CRT
+    function display () {
+        var data1 = CleanDataForm(input1)
+        var data2 = CleanDataForm(input2)
+
+        // Check GCD of b
+        if (data2.length === data1.length) {
+            if (gcd_more_than_two_numbers(data2) === 1) {
+                setData (x => findMinX(data2, data1, data1.length) )
+                setSucces (x => true)
+            }
+            else {
+                alert("Komponen-komponen b harus saling relatif prima")
+                setSucces (x => false)
+            }
+        }
+        else {
+            alert("Jumlah komponen b dan a tidak sama")
+            setSucces (x => false)
+        }
+        
+        setShow (x => true)
+    }
+
+    //Returning the html tag
     return (
         <div className = "container">
            <h1 className = "judul">Chinese Remainder Theorem</h1>
@@ -169,7 +161,7 @@ const FormCalc = () => {
                 
                 <button className = "button1" type = "submit" onClick = {display}>Calculate</button>
             </form>
-
+            
             <div id="myDIV">
             
                     {(show && succes)?
